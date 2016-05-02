61bbfe23a09099a3cd119bcc02dcb0e94bef2fd8
lucyhattersley <lucy.a.hattersley@gmail.com>
2016-05-02 20:51:23 +0100

    finished ex10

---
+++ ex10.sql
@@ -0,0 +1,10 @@
+SELECT * FROM pet;
+
+UPDATE pet SET name = "Zed's Dead Pet" WHERE id in (
+    SELECT pet.id
+    FROM pet, person_pet, person
+    WHERE
+    person.id = person_pet.person_id AND
+    pet.id = person_pet.pet_id AND
+    person.first_name = "Zed"
+);
